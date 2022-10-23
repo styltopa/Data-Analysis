fbb691ecbfca8ea8051e82436c6f3b9967bbdc3d
Stelios Topalidis <steltop12@gmail.com>
2022-10-23 16:09:22 +0300

    Commit of exercises 2.1 through 3.1

---
+++ Exercise_2_3.m
@@ -0,0 +1,33 @@
+clc;
+clear;
+
+n = 10000;
+mu = [0; 0];
+% for values of the covariance different 
+% from 0 meaning X and Y are , Var[X + Y] != Var[X] + Var[Y]
+step = 0.1;
+range = 0:step:1;
+sumOfVar = zeros(length(range), 1);
+varOfSum = zeros(length(range), 1);
+indexCounter = 1;
+
+for covValues = range
+    sigma = [1 covValues; covValues 1];
+    R = mvnrnd(mu, sigma, n);
+    x = R(:,1);
+    y = R(:,2);
+    z = x + y;  
+    sumOfVar(indexCounter) = var(x) + var(y);
+    varOfSum(indexCounter) = var(x+y);
+    indexCounter = indexCounter +1;
+end
+
+figure(1)
+plot(transpose(range), sumOfVar)
+hold on
+
+plot(transpose(range), varOfSum)
+hold off
+xlabel('Covariance')
+title('Variance of X+Y and variance of X plus variance of Y')
+legend('Var[X] + Var[Y]', 'Var[X+Y]');
