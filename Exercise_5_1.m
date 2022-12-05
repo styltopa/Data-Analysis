ab15eb618764af56e29cb3fdc389f080e2129f9b
styltopa <steltop12@gmail.com>
2022-12-05 20:32:27 +0200

    Wrong Exercise 5.2 uploaded

--- Exercise_5_1.m
+++ Exercise_5_1.m
@@ -8,11 +8,11 @@ close all;
 
 % some value for reproducibility (so that consistent results are given for 
 % different values of n (part c))
-rng(454);
+% rng(454);
 %% For (c) comment/uncomment one of the option below to select the number of
 %% observations in each sample.
-[M, n] = deal(1000, 20);
-% [M, n] = deal(1000, 200);
+% [M, n] = deal(1000, 20);
+[M, n] = deal(1000, 200);
 [sigmaX, sigmaY, muX, muY] = deal(1, 1, 0, 0);
 muV = [muX, muY];
 % the actual rho between X and Y, and their true covariance
@@ -40,14 +40,8 @@ for j = 1:length(rho)
         jointSample = mvnrnd(muV, sigmaMat, n);
         X = jointSample(:, 1);
         Y = jointSample(:, 2);
-        % sample covariance matrix
-        sCovMat = cov(X, Y);
-        % extraction of sample stds and covariance from the covariance matrix
-        sSigmaX = sqrt(sCovMat(1, 1));
-        sSigmaY = sqrt(sCovMat(2, 2));
-        sCovXY = sCovMat(1, 2);
-        % sample correlation coefficient
-        sRho(i, j) = sCovXY/(sSigmaX*sSigmaY);    
+        corrCoefMat = corrcoef(X, Y);
+        sRho(i, j) = corrCoefMat(1, 2);
     end
 
     % sRhoFisher follows a normal distribution and therefore tests for its mean
@@ -60,25 +54,27 @@ for j = 1:length(rho)
     sRhoLow = (exp(2*sRhoFisherLow)-1)./(exp(2*sRhoFisherLow)+1);
     sRhoHigh = (exp(2*sRhoFisherHigh)-1)./(exp(2*sRhoFisherHigh)+1);
 
-    rhoInsideTheCICounter = 0;
+    
+    rejectionCounter = 0;
     for i = 1:M
-        if rho(j) > sRhoLow(i) && rho(j) < sRhoHigh(i)
-            rhoInsideTheCICounter = rhoInsideTheCICounter + 1;
+        if rho(j) < sRhoLow(i) || rho(j) > sRhoHigh(i)
+            rejectionCounter = rejectionCounter + 1;
         end
     end
 
-    rhoInsideTheCIPercent = rhoInsideTheCICounter/M;
+    rejectionPercent = rejectionCounter/M;
 
     if j == 1
+        fprintf('For n = %d observations per sample:\n', n);
         fprintf('(a)\n');
     end
-    fprintf(['For rho = %.2f:\nPercentage of rho being inside the ',...
+    fprintf(['For rho_(X,Y) = %.2f:\nPercentage of rho being inside the ',...
         'ci:\n'], rho(j));
     % theoretical meaning taken from the parametric confidence intervals
     % see variable critVal 
     fprintf('Theoretical: %.2f%%\n', 100*(1-alpha));
     fprintf('Sample: %.2f%%  (M = %d samples)\n\n', ...
-        100*rhoInsideTheCIPercent, M);
+        100*(1-rejectionPercent), M);
 
     figure(j);
     histogram(sRhoLow);
@@ -102,44 +98,75 @@ for j = 1:length(rho)
         num2str(rho(j))], 'Interpreter', 'latex');
 end
 
+% Notes on (a): the fisher transform gives rejection rates (or non
+% rejection rates - it's the same) similar to the expected: (1-alpha)%
+
 
 %% (b) Hypothesis testing of correlation coefficient of X, Y equal to zero
 %% null hypothesis is expected to be rejected (alpha)% of the times
+rhoStudentised = rho.*sqrt((M-2)./(1-rho.^2));
 
 % for the different values of rho that we want to check
 for j = 1:length(rho)
     
     % transform the sample rho to follow the student distribution
-    sRhoStudentTransformed = sRho(:, j).*sqrt((M-2)./(1-sRho(:, j).^2));
+    sRhoStudentised = sRho(:, j).*sqrt((M-2)./(1-sRho(:, j).^2));
 %     [h, p] = ttest(sRhoStudentTransformed, rho(j));
     
-    [h, p] = ttest(sRhoStudentTransformed, ...
+    [h, p] = ttest(sRhoStudentised, ...
         rho(j)*sqrt((M-2)./(1-rho(j)^2)));
     if j == 1
         fprintf('(b)\n');
     end
-    fprintf('For rho = %.2f:\n', rho(j));
+    fprintf('For rho_(X,Y) = %.2f:\n', rho(j));
     figure(length(rho) + j);
-    histogram(sRhoStudentTransformed);
-    fprintf(['The null hypothesis that rho = %.2f can be rejected \n',...
-    '(p-value) %.4f%% of the times.\n\n'], rho(j),...
-    100*p);
+    histogram(sRhoStudentised);
+    title(['M = ', num2str(M), ' samples of studentised $r_{X,Y}$'], ...
+        ['$\rho_{X,Y}$ = ', num2str(rho(j))], 'interpreter', 'latex');
+    xline(rhoStudentised(j), '-', {'actual rho (studentised)'});
+    fprintf(['The null hypothesis that rho = %.2f cannot be rejected \n',...
+    '(1 - p-value) %.4f%% of the times.\n\n'], rho(j),...
+    100*(1-p));
+    if j == length(rho)
+        fprintf('---------------------\n');
+    end
 end
 
-% Notes: 
-% 1) the p-value (percentage of rejection of the null hypothesis) changes 
-% in every iteration 
-% of the experiment and is not consistent as far as the relative rejection
-% rate goes (the rejection rate for rho=0 might be higher than for rho=0.5 
-% one time and the smaller the next one).
-% 2) Even when rng(0) for reproducibility is introduced, there is still not
-% the convergence to alpha expected. (different seeds give different
-% results for n=20 and n=200
+% Notes on (b): 
+% 1) For n = 20, the hypothesis that rho_X,Y = 0 gives incosistent 
+% p-values meaning incosistent rejection rates of the hypothesis.
+% The hypothesis that rho_X,Y = 0.5 gives more stable rejection rates
+% (higher than the ones with the fisher transform)
+
+% 2) For n = 200, the results of the rejection rates are totally
+% incosistent. Some executions of the program result in close rejection
+% rates to the ones 
+
+% Notes on (c):
+% The results are not consistent as far as the increase in n goes.
+% What I mean by this is that it is expected that more observations per
+% sample would account for rejection rates converging to the real ones.
+% This does not seem to be the case (probably some error of the
+% calculations?).
 
 
-%% (d) 
+%% (d)
+% some value for reproducibility (so that consistent results are given for 
+% different values of n (part c))
+% rng(454);
+%% For (c) comment/uncomment one of the option below to select the number of
+%% observations in each sample.
+% [M, n] = deal(1000, 20);
+[M, n] = deal(1000, 200);
+[sigmaX, sigmaY, muX, muY] = deal(1, 1, 0, 0);
+muV = [muX, muY];
+% the actual rho between X and Y, and their true covariance
+% rho can be however long vector we want
+rho = [0, 0.5];
+covXY = rho*sigmaX*sigmaY;
 
-%% (a) (1-alpha)% Theoretical and sample confidence interval 
+
+%% (d.a) (1-alpha)% Theoretical and sample confidence interval 
 %% for the correlation coefficient rho
 % sample rho
 sRho = nan(M, length(rho));
@@ -156,16 +183,10 @@ for j = 1:length(rho)
     % calculate the sample rho from all M samples
     for i = 1:M
         jointSample = mvnrnd(muV, sigmaMat, n);
-        X2 = jointSample(:, 1).^2;
-        Y2 = jointSample(:, 2).^2;
-        % sample covariance matrix
-        sCovMat2 = cov(X2, Y2);
-        % extraction of sample stds and covariance from the covariance matrix
-        sSigmaX2 = sqrt(sCovMat(1, 1));
-        sSigmaY2 = sqrt(sCovMat(2, 2));
-        sCovXY2 = sCovMat2(1, 2);
-        % sample correlation coefficient
-        sRho(i, j) = sCovXY/(sSigmaX2*sSigmaY2);    
+        X = jointSample(:, 1).^2;
+        Y = jointSample(:, 2).^2;
+        corrCoefMat = corrcoef(X, Y);
+        sRho(i, j) = corrCoefMat(1, 2);
     end
 
     % sRhoFisher follows a normal distribution and therefore tests for its mean
@@ -178,27 +199,29 @@ for j = 1:length(rho)
     sRhoLow = (exp(2*sRhoFisherLow)-1)./(exp(2*sRhoFisherLow)+1);
     sRhoHigh = (exp(2*sRhoFisherHigh)-1)./(exp(2*sRhoFisherHigh)+1);
 
-    rhoInsideTheCICounter = 0;
+    
+    rejectionCounter = 0;
     for i = 1:M
-        if rho(j) > sRhoLow(i) && rho(j) < sRhoHigh(i)
-            rhoInsideTheCICounter = rhoInsideTheCICounter + 1;
+        if rho(j) < sRhoLow(i) || rho(j) > sRhoHigh(i)
+            rejectionCounter = rejectionCounter + 1;
         end
     end
 
-    rhoInsideTheCIPercent = rhoInsideTheCICounter/M;
+    rejectionPercent = rejectionCounter/M;
 
     if j == 1
-        fprintf('(d.a)\nFor X^2 and Y^2\n');
+        fprintf('For n = %d observations per sample:\n', n);
+        fprintf('(d.a)\n');
     end
-    fprintf(['For X, Y having rho = %.2f:\nPercentage of rho being inside the ',...
+    fprintf(['For rho_(X,Y) = %.2f:\nPercentage of rho being inside the ',...
         'ci:\n'], rho(j));
     % theoretical meaning taken from the parametric confidence intervals
     % see variable critVal 
     fprintf('Theoretical: %.2f%%\n', 100*(1-alpha));
     fprintf('Sample: %.2f%%  (M = %d samples)\n\n', ...
-        100*rhoInsideTheCIPercent, M);
+        100*(1-rejectionPercent), M);
 
-    figure(4+j);
+    figure(2*length(rho)+j);
     histogram(sRhoLow);
     xline(mean(sRhoLow), '-', {'$$\bar{r}_{low}$'}, 'FontSize', 12,...
         'LineWidth', 2,...
@@ -220,27 +243,35 @@ for j = 1:length(rho)
         num2str(rho(j))], 'Interpreter', 'latex');
 end
 
+% Notes on (a): the fisher transform gives rejection rates (or non
+% rejection rates - it's the same) similar to the expected: (1-alpha)%
 
-%% (b) Hypothesis testing of correlation coefficient of X, Y equal to zero
+
+%% (d.b) Hypothesis testing of correlation coefficient of X, Y equal to zero
 %% null hypothesis is expected to be rejected (alpha)% of the times
+rhoStudentised = rho.*sqrt((M-2)./(1-rho.^2));
 
 % for the different values of rho that we want to check
 for j = 1:length(rho)
     
     % transform the sample rho to follow the student distribution
-    sRhoStudentTransformed = sRho(:, j).*sqrt((M-2)./(1-sRho(:, j).^2));
+    sRhoStudentised = sRho(:, j).*sqrt((M-2)./(1-sRho(:, j).^2));
 %     [h, p] = ttest(sRhoStudentTransformed, rho(j));
     
-    [h, p] = ttest(sRhoStudentTransformed, ...
+    [h, p] = ttest(sRhoStudentised, ...
         rho(j)*sqrt((M-2)./(1-rho(j)^2)));
     if j == 1
         fprintf('(d.b)\n');
     end
-    fprintf('For X, Y having rho = %.2f:\n', rho(j));
-    figure(4 + length(rho) + j);
-    histogram(sRhoStudentTransformed);
-    fprintf(['The null hypothesis that rho = %.2f can be rejected \n',...
-    '(p-value) %.4f%% of the times.\n\n'], rho(j),...
-    100*p);
+    fprintf('For rho_(X,Y) = %.2f:\n', rho(j));
+    figure(3*length(rho) + j);
+    histogram(sRhoStudentised);
+    title(['M = ', num2str(M), ' samples of studentised $r_{X^2,Y^2}$'], ...
+        ['$\rho_{X,Y}$ = ', num2str(rho(j))], 'interpreter', 'latex');
+    xline(rhoStudentised(j), '-', {'actual rho (studentised)'});
+    fprintf(['The null hypothesis that rho = %.2f cannot be rejected \n',...
+    '(1 - p-value) %.4f%% of the times.\n\n'], rho(j),...
+    100*(1-p));
 end
 
+% Notes on (d):
