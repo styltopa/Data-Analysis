10f014290ecbe90d929efdb2bbaaea8c32c74b52
styltopa <steltop12@gmail.com>
2023-01-15 23:52:09 +0200

    All exercises of Chapter 2 added to a single folder

---
+++ Chapter 2/Exercise_2_4.m
@@ -0,0 +1,41 @@
+% Stelios Topalidis
+% AEM: 9613
+% Exercise 2.4
+
+clear;
+clc;
+
+[xmin, xmax] = deal(1, 2); %Case 1
+% [xmin, xmax] = deal(0, 1); %Case 2
+% [xmin, xmax] = deal(-1, 1); %Case 3
+[nmin, nmax, nstep]  = deal(10, 100000, 1000);
+nrange = nmin:nstep:nmax;
+
+[oneOverMeanOfX , meanOfOneOverX] = deal(zeros(length(nrange), 1)); 
+nCounter = 1;
+ 
+X = xmin + (xmax-xmin)*rand(nmax, 1);
+
+for n = nrange
+    oneOverMeanOfX(nCounter) = 1/mean(X(1:n));
+    meanOfOneOverX(nCounter) = mean(1/X(1:n));
+    nCounter = nCounter + 1;
+end
+
+figure(1)
+horAxis = transpose(nrange);
+plot(horAxis, meanOfOneOverX, 'LineWidth', 2)
+hold on
+plot(horAxis, oneOverMeanOfX, 'LineWidth', 2)
+hold off
+legend('E[1/X]', '1/E[X]');
+titleStr = sprintf(['1/E[X] and E[1/X] for various ', ...
+    'numbers of observations(n)']);
+subtitleStr = sprintf('X ~ U(%d, %d)', xmin, xmax);
+title(titleStr, subtitleStr);
+xlabel('Number of observations n')
+ylabel('E[1/X], 1/E[X]');
+
+lengthOfMeanMatrices = length(nrange);
+fprintf('For n = %d:\n1/E[X] = %.4f\nE[1/X] = %d\n', nmax, ...
+    oneOverMeanOfX(lengthOfMeanMatrices), meanOfOneOverX(lengthOfMeanMatrices));
---
+++ Chapter 2/Exercise_2_6.m
@@ -0,0 +1,90 @@
+% Stelios Topalidis
+% AEM: 9613
+% Exercise 2.6
+
+clear;
+clc;
+
+%% Simulation of the central limit theorem
+% n is the number of variables
+n = 100;
+% N is the number of samples we get from each 
+% random variable X
+N = 10000;
+
+% i-th row: has N samples of only the i-th random variable
+% j-th column: the j-th sample (100x1 vector) of the n variables
+X = rand(n, N);
+% Y: mean of the all (N in number) 100x1 vectors of the variables in X
+Y = mean(X, 1);
+% simulation mean and variance
+sMuOfY = mean(Y);
+sVarOfY = var(Y);
+
+%% Theoretical analysis according to the the central limit theorem
+% - tpdf: theoretical pdf of Y
+% - a,b: value interval of the uniformly distributed 
+% random variables X
+[a, b] = deal(0, 1);
+% theoretical mean, variance and standard deviation
+tMuOfY = (a+b)/2; 
+tVarOfY = ((b-a)/12)/n; 
+tSigmaOfY = sqrt(tVarOfY);
+% we need around 1000 steps 
+% so that the fitting curve of the normal distibution 
+% is smooth
+ystep = 8*tSigmaOfY/1000;
+y = tMuOfY-3*tSigmaOfY:ystep:tMuOfY+3*tSigmaOfY;
+tpdf = exp((-(y-tMuOfY).^2)/(2*tVarOfY))./sqrt(2*pi*tVarOfY);
+
+%% Console output
+fprintf(['Theoretical central limit theorem:\n',...
+    'mean: %f\n',...
+    'variance: %f\n\n'],tMuOfY , tVarOfY);
+fprintf(['Simulation of the central limit theorem:\n', ...
+    'mean: %f\n', ...
+    'variance: %f\n\n'], sMuOfY, sVarOfY);
+
+%% Plot
+figure(1);
+histogram(Y, 'Normalization', 'pdf');
+subtitleStr = sprintf('For each variable X: E[X] = (a+b)/2 = %.2f, Var[X] = ((b-a)/12) = %f', tMuOfY, tVarOfY);
+title('Histogram of the mean value of 100 variables X~U(0,1)', subtitleStr);
+    
+hold on;
+plot(y, tpdf, 'Linewidth', 2); 
+hold off
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
