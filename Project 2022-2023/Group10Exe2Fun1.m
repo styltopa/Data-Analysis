ea0b308bee4419233bb2795ba358e01fc489747d
styltopa <steltop12@gmail.com>
2023-01-16 12:49:46 +0200

    Files now have the right contents

--- Project 2022-2023/Group10Exe2Fun1.m
+++ Project 2022-2023/Group10Exe2Fun1.m
@@ -1,10 +1,26 @@
-% Stylianos Topalidis
+% Function 1 for exercise 2% Stylianos Topalidis
 % AEM: 9613
 % Stamatis Harteros
-% AEM: 
+% AEM: 9516
 % Project for academic year 2022-2023
-% Exercise 1
+% Function 1 for Exercise 2
 
 clc;
 clear;
 close all;
+
+data = table2array(readtable('Heathrow.xlsx'));
+
+data3 = data(:, 3);
+[ci, ciBoot] = confidenceCalc(data3);
+function [CI ,CIbootstrap] = confidenceCalc(x)
+% 	 removing Nan values
+    idx  = isnan(x);
+    x(idx) = [];
+
+    alpha = 0.05;
+    [~, ~, CI, ~] = ttest(x, mean(x), 'Alpha', alpha);
+    
+    nboot = 1000;
+    CIbootstrap = bootci(nboot, @mean, x);
+end
