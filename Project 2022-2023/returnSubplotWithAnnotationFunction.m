6745821a50fd8458397ed0a372468bdf4db9674b
styltopa <steltop12@gmail.com>
2023-01-28 17:22:25 +0200

    Function that returns an annotation box given the axis of the figure or plot or subplot or tile in a tiledlayout

---
+++ Project 2022-2023/returnSubplotWithAnnotationFunction.m
@@ -0,0 +1,48 @@
+close all;
+clc;
+clear;
+
+n = 100;
+m = 10;
+totalLayouts = 1;
+
+for numOfLayouts = 1:totalLayouts
+    figure(numOfLayouts);
+    tiledlayout(2, 2);
+    sgtitle("Scatter diagrams " + num2str(numOfLayouts));
+
+    for i = 1:4
+        ax = nexttile;
+        disp(ax.InnerPosition);
+        x = rand(n, 1);
+        y = rand(n, 1);
+        returnSubplot(x, y, m, ax);
+        title("Subplot " + num2str(i));
+    end
+end
+
+function returnSubplot(x, y, m, ax)
+    scatter(x, y);
+    hold on;
+    plot(x(1:m), y(1:m));
+    hold off;
+    adjR2 = rand(1,1);
+    annotationFontSize = 12;
+%     posArr = ax.OuterPosition;
+    posArr = ax.InnerPosition;
+    xOffset = 0.01;
+    yOffset = 0.01;
+    [posX, posY, width, height] = deal(posArr(1) - xOffset , ...
+        posArr(2) + xOffset ,...
+        0.1 ,...
+        0.1); 
+    annotPosAndDims = [posX, posY, width, height];
+%     annotPosAndDims = [ax.xlim, ax.ylim];
+    annotation('textbox', annotPosAndDims, 'String', ...
+        {"$AdjR^2$: "+ adjR2}, ...
+        'interpreter', 'latex', ...
+        'FontSize', ...
+        annotationFontSize);
+end
+
+
