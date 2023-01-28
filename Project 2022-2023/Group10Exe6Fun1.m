59dfb2bbb723b5a30d2a61040123df0738d82ecb
styltopa <steltop12@gmail.com>
2023-01-28 17:22:49 +0200

    Minor fixes

--- Project 2022-2023/Group10Exe6Fun1.m
+++ Project 2022-2023/Group10Exe6Fun1.m
@@ -6,7 +6,7 @@
 % Function for exercise 6
 
 
-function adjR2 = Group10Exe6Fun1(x, y, xName, yName, xNameP, yNameP)
+function adjR2 = Group10Exe6Fun1(x, y, xName, yName, xNameP, yNameP, ax)
     %% (a) Remove Nan pair values
     
     % namesPeriphrastically = [Mean annual tempearature
@@ -29,25 +29,25 @@ function adjR2 = Group10Exe6Fun1(x, y, xName, yName, xNameP, yNameP)
 
     dotSize = 15;
     scatter(x, y, dotSize, 'filled');
-%     title({'Scatter diagram between:' ; 
-%         xNameP + " (" + xName + ")" + ...
-%         " and " + yNameP + " (" + yName + ")" });
-      title({'Scatter diagram between:' ; 
-         xName + ...
-        " and " + yName});
-    
+    % the title regards the independent variable (horizontal axis)
+    title({xNameP + " (" + xName + ")"});    
     xlabel(xName);
     ylabel(yName);
     hold on;
     plot(x, yModel, 'Color', 'r', 'LineWidth', 1.5);
-%     annotationFontSize = 12;
-%     [posX, posY, width, height] = deal(0.65, 0.75, 0.1, 0.1); 
-%     annotPosAndDims = [posX, posY, width, height];
-%     annotation('textbox', annotPosAndDims, 'String', ...
-%         {"$AdjR^2$: "+ adjR2}, ...
-%         'interpreter', 'latex', ...
-%         'FontSize', ...
-%         annotationFontSize);
+    
+%     xlim(xlim+[-10,10]);ylim(ylim+[-10,10]);%use xlim and ylim to fix location
+%     text(min(xlim), max(ylim),subPlotNames(i), 'Horiz','left', 'Vert','top')
+%     
+    annotationFontSize = 12;
+    [posX, posY, width, height] = deal(0.65, 0.75, 0.1, 0.1); 
+    annotPosAndDims = [posX, posY, width, height];
+%     annotPosAndDims = [ax.xlim, ax.ylim];
+    annotation('textbox', annotPosAndDims, 'String', ...
+        {"$AdjR^2$: "+ adjR2}, ...
+        'interpreter', 'latex', ...
+        'FontSize', ...
+        annotationFontSize);
     hold off
 end
 
