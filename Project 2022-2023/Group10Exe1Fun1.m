d6634686a8055e0aa6c5a811f4fe7a8e91533371
styltopa <steltop12@gmail.com>
2023-02-03 16:06:13 +0200

    Exercise 1 working

--- Project 2022-2023/Group10Exe1Fun1.m
+++ Project 2022-2023/Group10Exe1Fun1.m
@@ -5,47 +5,35 @@
 % Project for academic year 2022-2023
 % Function 1 for exercise 1
 
-clc;
-clear;
-close all;
-
-data = table2array(readtable('Heathrow.xlsx'));
-
-sampleSize = 100;
-% the number of unique values should be the quite high for the p-value to
-% be computed.
-intRange = 9;
-intOffset = 1;
-% values from intOffset to intOffset + range
-smallV = round(intRange *rand(sampleSize , 1)) + intOffset;
-largeV = randn(sampleSize , 1);
-
-% Vector samplesize cases (> 10 or < 10)
-v = smallV;
-%v = largeVSet; 
-% v = rmmissing(data(:, 12));
+function [p1,p2] = Group10Exe1Fun1(data)
+v=unique(data);
 
 
-% The discrete values of the vector 
-vSet = unique(v, 'sorted');
-
-
-if length(vSet) <= 10
-    probabilityOfSuccess = mean(v)/365;
+if length(v) <= 10
+    probabilityOfSuccess = mean(data)/365;
     numberOfTrials = 365;
-    [hBinomial, pBinomial] = chi2gof(v,'CDF',...
-        {@binocdf, numberOfTrials, probabilityOfSuccess});
-    [hDiscreteUniform, pDiscreteUniform] = chi2gof(v,'cdf',...
-        {@unidcdf,max(v)});
+    [~, pBinomial] = chi2gof(data,'cdf',{@binocdf,numberOfTrials,probabilityOfSuccess});
+    [~, pDiscreteUniform] = chi2gof(data,'cdf',{@unidcdf,max(data)});
     figure();
-    X = categorical(v, vSet, cellstr(num2str(vSet)));
-    
-    histogram(X, 'BarWidth', 0.5);
-    title({'Bar graph of values in sample';...
-        [ 'p-value for binomial dist.: ', ...
+    bar(data);
+    title({'Bar graph of values in sample';[ 'p-value for binomial dist.: ', ...
         num2str(pBinomial)];...
-        ['p-value for discrete uniform dist.:',...
-        num2str(pDiscreteUniform)]});
-     xlabel('Values');
-    ylabel('Frequencies')
+        ['p-value for discrete uniform dist.:', num2str(pDiscreteUniform)]});
+    xlabel('index');
+    ylabel('Value')
+    p1 = pBinomial;
+    p2 = pDiscreteUniform;
+else
+    [~, pNormal] = chi2gof(data);
+    [~, pUniform] = chi2gof(data,'cdf',{@unifcdf,min(data),max(data)});
+    figure();
+    histogram(data);
+    title({'Histogram of values in sample';[ 'p-value for normal dist.: ', ...
+        num2str(pNormal)];...
+        ['p-value for uniform dist.:', num2str(pUniform)]});
+    xlabel('Values');
+    ylabel('Frequencies');
+    p1 = pNormal;
+    p2 = pUniform;
+end
 end
--- Project 2022-2023/Group10Exe1Prog1.m
+++ Project 2022-2023/Group10Exe1Prog1.m
@@ -1,25 +1,16 @@
 % Stylianos Topalidis
 % AEM: 9613
-% Stamatis Harteros
-% AEM: 9516
+% Stamatis Charteros
+% AEM:9516
 % Project for academic year 2022-2023
-% Exercise 1
-
+% Program for exercise 1
 clc;
-clear;
 close all;
 
 data = table2array(readtable('Heathrow.xlsx'));
 
 
-[ci, ciBoot] = confidenceCalc(data);
-function[CI ,CIbootstrap] = confidenceCalc(x)
-	idx  = isnan(x);
-    x(idx) = [];
-
-    alpha = 0.05;
-    [~, ~, CI, ~] = ttest(x, mean(x), 'Alpha', alpha);
-    
-    nboot = 1000;
-    CIbootstrap = bootci(nboot, @mean, x);
+p = zeros(11,2);
+for i = 2:12
+    [p(i,1),p(i,2)] = Group10Exe1Fun1(data(:,i));
 end
