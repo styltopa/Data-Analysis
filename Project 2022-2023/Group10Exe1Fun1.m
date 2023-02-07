3e10580bf02298f92e65b3dddeb7281763565bcc
styltopa <steltop12@gmail.com>
2023-02-07 19:34:54 +0200

    Exercise 1, program 1: added concole outputs for the decision of the followed distributions

--- Project 2022-2023/Group10Exe1Fun1.m
+++ Project 2022-2023/Group10Exe1Fun1.m
@@ -6,34 +6,38 @@
 % Function 1 for exercise 1
 
 function [p1,p2] = Group10Exe1Fun1(data)
-v=unique(data);
+    
+    v = rmmissing(data);
+    vSet = unique(v);
 
-
-if length(v) <= 10
-    probabilityOfSuccess = mean(data)/365;
-    numberOfTrials = 365;
-    [~, pBinomial] = chi2gof(data,'cdf',{@binocdf,numberOfTrials,probabilityOfSuccess});
-    [~, pDiscreteUniform] = chi2gof(data,'cdf',{@unidcdf,max(data)});
-    figure();
-    bar(data);
-    title({'Bar graph of values in sample';[ 'p-value for binomial dist.: ', ...
-        num2str(pBinomial)];...
-        ['p-value for discrete uniform dist.:', num2str(pDiscreteUniform)]});
-    xlabel('index');
-    ylabel('Value')
-    p1 = pBinomial;
-    p2 = pDiscreteUniform;
-else
-    [~, pNormal] = chi2gof(data);
-    [~, pUniform] = chi2gof(data,'cdf',{@unifcdf,min(data),max(data)});
-    figure();
-    histogram(data);
-    title({'Histogram of values in sample';[ 'p-value for normal dist.: ', ...
-        num2str(pNormal)];...
-        ['p-value for uniform dist.:', num2str(pUniform)]});
-    xlabel('Values');
-    ylabel('Frequencies');
-    p1 = pNormal;
-    p2 = pUniform;
-end
+    if length(vSet) <= 10
+        probabilityOfSuccess = mean(data)/365;
+        numberOfTrials = 365;
+        [~, pBinomial] = chi2gof(data, 'cdf', ...
+            {@binocdf,numberOfTrials,probabilityOfSuccess});
+        [~, pDiscreteUniform] = chi2gof(data,'cdf',{@unidcdf,max(data)});
+        figure();
+    %     bar(data);
+        X = categorical(data, vSet, cellstr(num2str(unique(vSet, 'sorted'))));
+        histogram(X, 'BarWidth', 0.5);
+        title({'Bar graph of values in sample'; ['p-value for binomial dist.: ', ...
+            num2str(pBinomial)];...
+            ['p-value for discrete uniform dist.: ', num2str(pDiscreteUniform)]});
+        xlabel('Values');
+        ylabel('Frequency')
+        p1 = pBinomial;
+        p2 = pDiscreteUniform;
+    else
+        [~, pNormal] = chi2gof(data);
+        [~, pUniform] = chi2gof(data,'cdf',{@unifcdf,min(data),max(data)});
+        figure();
+        histogram(data);
+        title({'Histogram of values in sample';[ 'p-value for normal dist.: ', ...
+            num2str(pNormal)];...
+            ['p-value for uniform dist.: ', num2str(pUniform)]});
+        xlabel('Values');
+        ylabel('Frequencies');
+        p1 = pNormal;
+        p2 = pUniform;
+    end
 end
--- Project 2022-2023/Group10Exe1Prog1.m
+++ Project 2022-2023/Group10Exe1Prog1.m
@@ -7,10 +7,70 @@
 clc;
 close all;
 
+
 data = table2array(readtable('Heathrow.xlsx'));
+dataNamesStruct = importdata('Heathrow.xlsx');
+dataNames = string(dataNamesStruct.textdata.Sheet1);
+dataNamesPeriphrastic = {'Year', 'Mean annual temperature', ...
+        'Mean annual maximum temperature', 'Mean annual minimum temperature', ...
+        'Total annual rainfall or snowfall', 'Mean annual wind velocity', ...
+        'Number of days with rain', 'Number of days with snow', ...
+        'Number of days with wind', 'Number of days with fog', ...
+        'Number of days with tornado', 'Number of days with hail'};
 
-
+alpha = 0.05;
 p = zeros(11,2);
-for i = 2:12
-    [p(i,1),p(i,2)] = Group10Exe1Fun1(data(:,i));
+fprintf(['Tests if the feature follows given distributions \n',...
+    'and length of unique values in input vector.\n',...
+    '(uniform or normal\n',...
+    'distribution for large number of unique values and the binomial ',...
+    'and discrete \n',...
+    'uniform for small number of unique values of the input vector)\n']); 
+fprintf('The tests are held at a %.2f significance level\n\n', alpha);
+for i = 1:11
+    fprintf('%s (%s) follows the:\n', dataNames(i+1), ...
+        string(dataNamesPeriphrastic(i+1)));
+    dataV = data(:,i+1);
+    dataUniqueSet = unique(dataV);
+    [p(i,1),p(i,2)] = Group10Exe1Fun1(dataV);
+    
+%   Test for the discrete distributions
+%   (large number of unique values in the input vector)
+    if size(dataUniqueSet, 1) <= 10
+        if p(i, 1) < alpha
+            fprintf('- Binomial: No\n');
+        elseif p(i, 1) > alpha
+            fprintf('- Binomial: Yes\n');
+        elseif isnan(p(i, 1))
+            fprintf('- Binomial: Test returns Nan for the p-value\n');
+        end
+        if p(i, 2) < alpha
+            fprintf('- Discrete uniform: No\n');
+        elseif p(i, 2) > alpha
+            fprintf('- Discrete uniform: Yes\n');
+        elseif isnan(p(i, 2))
+            fprintf(['- Discrete uniform: Test returns Nan for ',...
+                'the p-value\n']);
+        end
+%   Test for the continuous distributions 
+%   (large number of unique values in the input vector)
+    elseif size(dataUniqueSet, 1) > 10
+        if p(i, 1) < alpha
+            fprintf('- Normal: No\n');
+        elseif p(i, 1) > alpha
+            fprintf('- Normal: Yes\n');
+        elseif isnan(p(i, 1))
+            fprintf('- Normal: Test returns Nan for the p-value\n');
+        end
+        if p(i, 2) < alpha
+            fprintf('- Uniform: No\n');
+        elseif p(i, 2) > alpha
+            fprintf('- Uniform: Yes\n');
+        elseif isnan(p(i, 2))
+            fprintf(['- Uniform: Test returns Nan for ',...
+                'the p-value\n']);
+        end
+    end
+    
+    fprintf('-------------------------\n');
 end
