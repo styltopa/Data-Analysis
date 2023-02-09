2cb3348860dd74f722f051e7efd9534bb7ba1e8a
styltopa <steltop12@gmail.com>
2023-02-09 20:30:17 +0200

    Exercise 7: Added labels to the axes plots

--- Project 2022-2023/Group10Exe7Fun1.m
+++ Project 2022-2023/Group10Exe7Fun1.m
@@ -6,7 +6,7 @@
 % Function for exercise 7
 
 
-function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
+function [adjR2Max , Index] = Group10Exe7Fun1(x, y, xName, xNameP, yName)
     
     % Remove NaN values
     xAndY = [x, y];  
@@ -21,7 +21,7 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
     yModel1 = yModelStruct1.Fitted;
     adjR2_1 = yModelStruct1.Rsquared.Adjusted;
 
-    [X,I] =sort(x);
+    [xSorted,I] =sort(x);
     Y = yModel1(I);
     
     figure();
@@ -30,11 +30,12 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
     scatter(x, y, dotSize, 'filled');
     hold on;
     title('1st degree polynomial (linear) model');
-    plot(X, Y, 'Color', 'r', 'LineWidth', 1.5);
-        
+    plot(xSorted, Y, 'Color', 'r', 'LineWidth', 1.5);
+    xlabel(xName);
+    ylabel(yName);
     annotationFontSize = 12;
     posArr = ax.InnerPosition;
-    [xOffset, yOffset] = deal(0.1, 0.12);
+    [xOffset, yOffset] = deal(0.1, 0.14);
     [posX, posY, width, height] = deal(posArr(1) + posArr(3) - xOffset, ...
     posArr(2) + posArr(4) - yOffset, 0.1 ,0.1); 
     annotPosAndDims = [posX, posY, width, height];
@@ -48,8 +49,8 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
 
     %% 2nd degree polynomial model
     
-    x2  =[x, x.^2];
-    yModelStruct2 = fitlm(x2, y, 'RobustOpts', 'ols');
+    X  =[x, x.^2];
+    yModelStruct2 = fitlm(X, y, 'RobustOpts', 'ols');
     yModel2 = yModelStruct2.Fitted;
     adjR2_2 = yModelStruct2.Rsquared.Adjusted;
 
@@ -59,8 +60,9 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
     scatter(x, y, dotSize, 'filled');
     hold on;
     title('2nd degree polynomial model');
-    plot(X, Y, 'Color', 'r', 'LineWidth', 1.5);
-    
+    plot(xSorted, Y, 'Color', 'r', 'LineWidth', 1.5);
+    xlabel(xName);
+    ylabel(yName);
     posArr = ax.InnerPosition;
     [posX, posY, width, height] = deal(posArr(1) + posArr(3) - xOffset, ...
     posArr(2) + posArr(4) - yOffset, 0.1 ,0.1); 
@@ -75,20 +77,20 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
 
     %% 3rd degree polynomial model 
     
-    x3  =[x, x.^2, x.^3];
+    X  =[x, x.^2, x.^3];
 
-    yModelStruct3 = fitlm(x3, y, 'RobustOpts', 'ols');
+    yModelStruct3 = fitlm(X, y, 'RobustOpts', 'ols');
     yModel3 = yModelStruct3.Fitted;
     adjR2_3 = yModelStruct3.Rsquared.Adjusted;
 
     ax=subplot(2,3,3);
-    dotSize = 15;
     scatter(x, y, dotSize, 'filled');
     hold on;
     
-    
     Y = yModel3(I);   
-    plot(X, Y, 'Color', 'r', 'LineWidth', 1.5);
+    plot(xSorted, Y, 'Color', 'r', 'LineWidth', 1.5);
+    xlabel(xName);
+    ylabel(yName);
     posArr = ax.InnerPosition;
     [posX, posY, width, height] = deal(posArr(1) + posArr(3) - xOffset, ...
     posArr(2) + posArr(4) - yOffset, 0.1 ,0.1); 
@@ -124,7 +126,6 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
 
     yModelStructTransformed = fitlm(xTransformed, yTransformed, ...
         'RobustOpts', 'ols');
-    yModelTransformed = yModelStructTransformed.Fitted;
 
 
     bTransformed = yModelStructTransformed.Coefficients.Estimate;
@@ -133,23 +134,22 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
 
 
     yModel = b(1).*xNonZeros.^b(2);
-    [X,I] =sort(xNonZeros);
+    [xSorted,I] =sort(xNonZeros);
 
     for i=1:length(I)
-    Y1(i)=yModel(I(i));
+        Y1(i)=yModel(I(i));
     end
 
 
-%     adjR2_4=1-((length(y)-1)/(length(y)-2))*((sum((y-yModel).^2)/sum((y-mean(y)).^2)));
     adjR2_4=1-((length(yNonZeros)-1)/(length(yNonZeros)-2))*((sum((yNonZeros-yModel).^2)/sum((yNonZeros-mean(yNonZeros)).^2)));
     
 
-    dotSize = 15;
     ax = subplot(2,3,4);
     scatter(x, y, dotSize, 'filled');
-
+    xlabel(xName);
+    ylabel(yName);
     hold on;
-    plot(X,Y1, 'Color', 'r', 'LineWidth', 1.5);
+    plot(xSorted, Y1, 'Color', 'r', 'LineWidth', 1.5);
     posArr = ax.InnerPosition;
     [posX, posY, width, height] = deal(posArr(1) + posArr(3) - xOffset, ...
     posArr(2) + posArr(4) - yOffset, 0.1 ,0.1); 
@@ -186,7 +186,7 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
 
     % revert beck to the linear model by changing the coefficients
     b = yModelStructTransformed.Coefficients.Estimate; 
-    [X, I] =sort(xNonZeros);
+    [xSorted, I] =sort(xNonZeros);
     for i =1:length(xNonZeros)
         yModel(i) = b(1)+b(2)/xNonZeros(i);
     end
@@ -196,12 +196,13 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
     adjR2_5=1-((length(yNonZeros)-1)/(length(yNonZeros)-2))*((sum((yNonZeros-yModel).^2)/sum((yNonZeros-mean(yNonZeros)).^2)));
 
 
-    dotSize = 15;
     ax = subplot(2,3,5);
     scatter(x, y, dotSize, 'filled');
     title('Instrinsically linear logarithmic model');
     hold on;
-    plot(X, Y2, 'Color', 'r', 'LineWidth', 1.5);
+    plot(xSorted, Y2, 'Color', 'r', 'LineWidth', 1.5);
+    xlabel(xName);
+    ylabel(yName);
 
     posArr = ax.InnerPosition;
     [posX, posY, width, height] = deal(posArr(1) + posArr(3) - xOffset, ...
@@ -215,7 +216,7 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
 
     hold off;
 
-    %% Exponential Intrinsically linear model
+    %% Exponential intrinsically linear model
 
     xNonZeros = x;
     yNonZeros = y;
@@ -229,14 +230,13 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
     end
 
     yModelStructTransformed = fitlm(xNonZeros, yTransformed, 'RobustOpts', 'ols');
-    yModelTransformed = yModelStructTransformed.Fitted;
 
-    % revert beck to the linear model by changing the coefficients
+    % Revert back to the linear model by changing the coefficients
     bTransformed = yModelStructTransformed.Coefficients.Estimate;
     b = bTransformed;
     b(1) = exp(bTransformed(1));
 
-    [X, I] =sort(x);
+    [xSorted, I] =sort(x);
     for i=1:length(x)
         yModel(i) = b(1)*exp(b(2)*x(i));
     end
@@ -247,13 +247,13 @@ function [adjR2Max , Index] = Group10Exe7Fun1(x,y,xName,xNameP)
     adjR2_6=1-((length(y)-1)/(length(y)-2))*((sum((y-yModel).^2)/sum((y-mean(y)).^2)));
 
 
-    dotSize = 15;
     ax = subplot(2,3,6);
     scatter(x, y, dotSize, 'filled');
     title('Intrinsically linear exponential model');
     hold on;
-    plot(X, Y, 'Color', 'r', 'LineWidth', 1.5);
-
+    plot(xSorted, Y, 'Color', 'r', 'LineWidth', 1.5);
+    xlabel(xName);
+    ylabel(yName);
     posArr = ax.InnerPosition;
     [posX, posY, width, height] = deal(posArr(1) + posArr(3) - xOffset, ...
     posArr(2) + posArr(4) - yOffset, 0.1 ,0.1); 
